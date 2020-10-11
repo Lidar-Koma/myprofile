36664e486a9c03b51db72bce73b11842fe476dd3
Lidar-koma <lidarkoma@gmail.com>
2020-10-11 17:13:21 -0500

    deploy my profile react github2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import profileImage from './img/lidar.jpg';
+import HeaderComponent from './HeaderComponent';
+import HomeComponent from './HomeComponent';
+import FooterComponent from './FooterComponent';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+    <HeaderComponent />
       <h1>My Profile</h1>
       <h2>Auther: lidar koma</h2>
       <img src={profileImage} alt="profile" />
+      <Router>
+                {<HeaderComponent  />}
+                <div className="c">
+                <Switch>
+                <Route path = "/myprofile/home" exact render={(props) => (
+                  <HomeComponent{...props} isSlide={true} options={{title: 'Home Page'}}/>
+               )}
+            />
+            <Route path = "/home1" exact render={(props) => (
+              <HomeComponent{...props} isSlide={true} options={{title: 'Home Page'}}/>
+           )}
+        />
+              
+                {/*<Route path = "/update-employee/:firstName" render={(props) => (
+                   <UpdateEmployeeComponent{...props} isSlide={true} />
+                )}
+                />*/}
+            
+            
+                </Switch>
+                </div>
+                <FooterComponent />
+                
+                </Router>
+                
     </div>
   );
 }
---
+++ src/HomeComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+export default class HomeComponent extends Component {
+
+    
+
+
+    render(props) {
+        //const isSlide = this.props.slide;
+        return (
+            <div>
+            <h1>Home page</h1>
+            </div>
+
+        )
+        }
+    }
